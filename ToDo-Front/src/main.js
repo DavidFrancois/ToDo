82fb0a0454d51d8ef2969f622fcb9ea225a578e1
DESKTOP-EPCPNTK\Vince <vincent.grandiere@gmail.com>
2017-01-28 15:14:02 +0100

    sauvegard

--- ToDo-Front/src/main.js
+++ ToDo-Front/src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import SignIn from './components/Sign-In.vue';
+import SignUp from './components/Sign-Up.vue';
+
+
+Vue.component('app-sign-in', SignIn);
+Vue.component('app-sign-up', SignUp);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
