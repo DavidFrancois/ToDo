052801cb0c502b38b14e7cee4363e9ae1c0f57ed
DESKTOP-EPCPNTK\Vince <vincent.grandiere@gmail.com>
2017-01-28 18:55:35 +0100

    Creation du template de page d'accueil / connection

---
+++ ToDo-Front/src/components/Sign-Up.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="sign-up-form">
+    <div class="form-group">
+      <label for="sign-up-login" >Login</label>
+      <input class="form-control" id="sign-up-login" placeholder="Login">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="sign-up-email">E-mail</label>
+        <input type="email"  class="form-control" id="sign-up-email" placeholder="E mail">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="sign-up-password">Mot de passe</label>
+        <input type="password"  class="form-control" id="sign-up-password" placeholder="Mot de passe">
+      </div>
+    </div>
+
+
+    <div class="form-group">
+      <label for="sign-up-verif-password" >Confirmer votre mot de passe :</label>
+      <input type="password"  class="form-control" id="sign-up-verif-password" placeholder="Confirmation mot de passe">
+    </div>
+
+    <div class="center-content"> <button  class="btn btn-primary"> Inscription </button> </div>
+  </form>
+</template>
+
+
+<style>
+  .sign-up-form{
+    width: 600px;
+    margin: auto;
+    margin-top: 100px;
+
+    background-color: rgb(250, 250, 250);
+
+    border-radius: 10px;
+    border-color: #f7f7f9;
+    border-style: solid;
+    padding: 20px;
+  }
+</style>
