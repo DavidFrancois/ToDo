d969a946da9eca9e66488cd526dd5eea0923f843
Guillaume Rousseau <roxons333@hotmail.fr>
2017-01-31 18:13:42 +0100

    création du component input

---
+++ toDoFront/src/components/inputs/Input.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="inputType=='text'">
+    <input type="text" v-bind:id="inputId" v-bind:placeholder="inputPlaceholder" v-bind:style="inputStyle" v-model="content" class="form-control">
+  </div>
+</template>
+
+<style scope>
+
+</style>
+
+<script type="text/javascript">
+    export default{
+      props:['input-type'],
+      data(){
+        return{
+          content:"",
+          inputStyle:""
+        }
+      },
+      methods:{
+        validate(){
+          this.inputStyle = 'border-color : green;';
+        },
+        invalidate(){
+          this.inputStyle = 'border-color : red;';
+        }
+      }
+    }
+</script>
