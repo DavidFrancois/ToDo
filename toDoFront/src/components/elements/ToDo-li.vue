fae1321b1e1cff15d9faed5b233cd1562e29117d
vince <vincent.grandiere@gmail.com>
2017-01-31 18:12:43 +0100

    premiere version toDoList

---
+++ toDoFront/src/components/elements/ToDo-li.vue
@@ -0,0 +1,28 @@
+<template >
+  <div class="to-do-li">
+    <span> {{toDoName}} </span>
+    <div class="btnGroup">
+      <button type="button" name="modif-toDo"> Modifier </button>
+      <button type="button" name="manage-user"> Gérer les accès </button>
+      <button type="button" name="del-toDo"> Supprimer </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['toDoName']
+}
+</script>
+
+<style scope>
+.to-do-li{
+  padding: 10px;
+  border-style: solid;
+  width: 500px;
+}
+
+.btnGroup{
+  text-align: right;
+}
+</style>
