60376d6a342106ca3dad5a0609f60479b72f8345
Francois David <francois.david107@gmail.com>
2017-02-01 22:56:47 +0100

    First steps on task interface

---
+++ toDoFront/src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="external-event bg-red ui-draggable ui-draggable-handle" style="position: relative;">{{ task }}</div>
+</template>
+
+<script>
+	export default {
+		name: 'Task',
+
+		props: ['task'],
+
+		data () {
+			return {
+				text: 'toto'
+			}
+		}
+	}
+</script>
