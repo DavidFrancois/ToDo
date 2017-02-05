0b1fa5bcb3bf4d4d6b9fd5627ce6c2902a587d62
Guillaume Rousseau <roxons333@hotmail.fr>
2017-02-05 15:41:14 +0100

    tests pour Sign-Up et Sign-In écrits et fonctionnels

--- toDoApi/app/controllers/common.js
+++ toDoApi/app/controllers/common.js
@@ -48,13 +48,35 @@ module.exports.update = function (Model) {
 
 module.exports.create = function (Model) {
     return function (req, res, next) {
+      if(validSignUp(req.body)){
         Model.create(req.body, function(err, model) {
             if (err) throw err;
-            res.writeHead(200,{
+            res.writeHead(201,{
               'Content-Type': 'application/json'
             });
             res.write(JSON.stringify(model));
             res.end();
         });
+      }
+      else{
+        res.writeHead(401,{
+          'Content-Type': 'text/plain'
+        });
+        res.write("Infos sent didn't match requirements");
+        res.end();
+      }
     }
+}
+
+var validSignUp = function(body){
+  let logex = new RegExp("^[a-zA-Z]{4,10}$");
+  let emailex = new RegExp("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+  let passwordex = new RegExp("^(?=.*[0-9])(?=.*[a-zA-Z]).{7,20}$");
+  if(!logex.test(body.login))
+    return false;
+  if(!emailex.test(body.email))
+    return false;
+  if(!passwordex.test(body.password))
+    return false;
+  return true;
 }
--- toDoApi/test/routes-tests.js
+++ toDoApi/test/routes-tests.js
@@ -1,27 +1,116 @@
+// launching the server
 process.env.NODE_ENV = 'test';
 var server = require('../server.js');
 
+// packages used to run tests
 var chai = require('chai');
 var expect = chai.expect;
 var request = require('supertest');
 var request = request('http://localhost:8080');
 
+// var needed to run tests
+  // Login infos
+var correctLogin = "test";
+var correctEmail = "test@test.test";
+var correctPassword = "test123";
+var wrongLogin = "t";
+var wrongEmail = "test";
+var wrongPassword = "test1";
+  // Paths
+var signUpPath = '/user';
+var signInPath = '/user/auth';
+  // Types
+var typeJson = 'json';
+var typeText = 'text/plain';
+
+// tests
 describe('Test backend API' , function () {
   describe('Test Sign-Up', function(){
-    it('return 200 and ID when Login, Email and Password are sent', function() {
+    it('returns 201 and ID when correct Login, Email and Password are sent', function() {
       return request
-        .post('/user')
-        .type("json")
+        .post(signUpPath)
+        .type(typeJson)
         .send({
-          login : 'test',
-          email : 'test',
-          password : 'test'
+          login : correctLogin,
+          email : correctEmail,
+          password : correctPassword
         })
-        .expect(200)
+        .expect(201)
         .then(response => {
           expect(response).to.not.be.undefined;
           expect(response.body).to.have.property('_id');
         });
+      });
+      it('returns 401 when wrong Login is sent', function(done) {
+        request
+          .post(signUpPath)
+          .type(typeJson)
+          .send({
+            login : wrongLogin,
+            email : correctEmail,
+            password : correctPassword
+          })
+          .expect(401)
+          .end(done);
+        });
+      it('returns 401 when wrong Email is sent', function(done) {
+        request
+          .post(signUpPath)
+          .type(typeJson)
+          .send({
+            login : correctLogin,
+            email : wrongEmail,
+            password : correctPassword
+          })
+          .expect(401)
+          .end(done);
+        });
+      it('returns 401 when wrong Password is sent', function(done) {
+        request
+          .post(signUpPath)
+          .type(typeJson)
+          .send({
+            login : correctLogin,
+            email : correctEmail,
+            password : wrongPassword
+          })
+          .expect(401)
+          .end(done);
+        });
+    });
+    describe('Test Sign-In', function(){
+      it('returns 200 when correct Login and Password are sent', function(done){
+        request
+        .get(signInPath)
+        .type(typeJson)
+        .query({
+          login : correctLogin,
+          password : correctPassword
+        })
+        .expect(200)
+        .end(done);
+      });
+      it('returns 401 when wrong Login is sent', function(done){
+        request
+        .get(signInPath)
+        .type(typeJson)
+        .query({
+          login : wrongLogin,
+          password : correctPassword
+        })
+        .expect(401)
+        .end(done);
+      });
+      it('returns 401 when wrong Password is sent', function(done){
+        request
+        .get(signInPath)
+        .type(typeJson)
+        .query({
+          login : correctLogin,
+          password : wrongPassword
+        })
+        .expect(401)
+        .end(done);
       });
     });
   });
