a2a28e5ffa1b0047275a642e3f579c32f47b9742
François David <francois.david107@gmail.com>
2017-02-21 15:48:33 +0100

    delete useless tab

--- toDoFront/src/components/ToDo-Menu.vue
+++ toDoFront/src/components/ToDo-Menu.vue
@@ -11,16 +11,6 @@
           </div>
         </div>    
       </div>
-      <div class="col-md-6">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h2>Autres todo</h2>  
-          </div>
-          <div class="panel-body">
-            <app-todo-li v-for="todo in myToDo" :toDoName="todo.name" :toDoID="todo._id"></app-todo-li>
-          </div>
-        </div>    
-      </div>
     </div>
   </div>
 </template>
